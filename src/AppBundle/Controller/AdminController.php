b683e3e0d85dfa4b983745e89bb2b8dbc2eed713
dzakiafif <dzakiafif12@gmail.com>
2017-11-24 13:30:23 +0700

    adding template for backend

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -9,8 +9,11 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Post;
 use AppBundle\Entity\User;
+use Cocur\Slugify\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller
@@ -30,11 +33,116 @@ class AdminController extends Controller
 
             return 'data berhasil disimpan';
         }
+
+        return $this->render('AppBundle:backend:register/register.html.twig');
     }
 
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        
+        $session = $request->getSession();
+        $em = $this->getDoctrine()->getManager();
+
+        if($request->getMethod() == 'POST') {
+            $username = $request->get('username');
+            $password = md5($request->get('password'));
+            
+            
+            $data = $em->getRepository(User::class)->findOneBy(['username'=>$username]);
+
+            if($data != null) {
+                if($data->getPassword() == $password) {
+                    $session->set('uname',['value'=>$data->getUsername()]);
+                }else {
+                    $this->get('session')->getFlashBag()->add(
+                        'message_error',
+                        'username/password salah'
+                    );
+                    return $this->redirect($this->generateUrl('popem_login'));
+                }
+            }else {
+                $this->get('session')->getFlashBag()->add(
+                    'message_error',
+                    'data tidak ditemukan'
+                );
+                return $this->redirect($this->generateUrl('popem_login'));
+            }
+            return $this->redirect($this->generateUrl('popem_admin_home'));
+        }
+
+        return $this->render('AppBundle:backend:login/login.html.twig');
+    }
+
+    public function logoutAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        $session->clear();
+
+        return $this->redirect($this->generateUrl(''));
+    }
+
+    public function postAction(Request $request)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $slugify = new Slugify();
+
+        if($request->getMethod() == 'POST') {
+            $data = new Post();
+            $data->setTitle($request->get('title'));
+            $data->setSlug($slugify->slugify($data->setTitle($request->get('title'))));
+            $data->setBody($request->get('body'));
+
+            if(!empty($request->files->get('image'))) {
+                $file = $request->files->get('image');
+                $name1 = md5(uniqid()) . '.' . $file->guessExtension();
+                $exAllowed = array('jpg','png','jpeg','svg');
+                $ex = pathinfo($name1, PATHINFO_EXTENSION);
+                if(in_array($ex,$exAllowed)) {
+                    if($file instanceof UploadedFile) {
+                        if(!$file->getClientSize() > (1024 * 1024 * 1)) {
+                            $data->setImage($name1);
+                        }else {
+                            return 'data tidak boleh lebih dari 1 mb';
+                        }
+                    }
+                }else{
+                    return 'extension harus .jpg, .png, .jpeg, .svg';
+                }
+            }
+
+            $arrNewTag = [];
+
+            foreach ($request->get('tag') as $item) {
+                array_push($arrNewTag, $item);
+            }
+
+            $data->setTag(serialize($arrNewTag));
+            $data->setMetaKeyword($request->get('meta-keyword'));
+            $data->setMetaDescription($request->get('meta-description'));
+
+            $em->persist($data);
+            $em->flush();
+
+            return 'data berhasil disimpan';
+        }
+
+        return $this->render('AppBundle:backend:post/new-post.html.twig');
+    }
+
+    public function listPostAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $data = $em->getRepository(User::class)->findAll();
+
+        return $this->render('AppBundle:backend:post/list-post.html.twig',['data'=>$data]);
+    }
+
+    public function homeAction()
+    {
+        return $this->render('AppBundle:backend:home/home.html.twig');
     }
 
 }
---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,229 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Post
+ */
+class Post
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $slug;
+
+    /**
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @var string
+     */
+    private $body;
+
+    /**
+     * @var string
+     */
+    private $tag;
+
+    /**
+     * @var string
+     */
+    private $metaKeyword;
+
+    /**
+     * @var string
+     */
+    private $metaDescription;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Post
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Post
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     *
+     * @return Post
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set tag
+     *
+     * @param string $tag
+     *
+     * @return Post
+     */
+    public function setTag($tag)
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    /**
+     * Get tag
+     *
+     * @return string
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
+
+    /**
+     * Set metaKeyword
+     *
+     * @param string $metaKeyword
+     *
+     * @return Post
+     */
+    public function setMetaKeyword($metaKeyword)
+    {
+        $this->metaKeyword = $metaKeyword;
+
+        return $this;
+    }
+
+    /**
+     * Get metaKeyword
+     *
+     * @return string
+     */
+    public function getMetaKeyword()
+    {
+        return $this->metaKeyword;
+    }
+
+    /**
+     * Set metaDescription
+     *
+     * @param string $metaDescription
+     *
+     * @return Post
+     */
+    public function setMetaDescription($metaDescription)
+    {
+        $this->metaDescription = $metaDescription;
+
+        return $this;
+    }
+
+    /**
+     * Get metaDescription
+     *
+     * @return string
+     */
+    public function getMetaDescription()
+    {
+        return $this->metaDescription;
+    }
+}
+
