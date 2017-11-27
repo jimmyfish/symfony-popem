49799eeedd7cd8aea275e276297b9dafa6ec6750
Jimmy Fish <killcoder212@gmail.com>
2017-11-27 17:28:13 +0700

    checkpoint at 17:28

--- src/AppBundle/Controller/ClientController.php
+++ src/AppBundle/Controller/ClientController.php
@@ -11,11 +11,27 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ClientController extends Controller
 {
     public function homeAction(Request $request)
     {
         return $this->render('AppBundle:Client:home/index.html.twig');
+    }
+
+    public function blogAction()
+    {
+        return new Response('BLOG VIEW');
+    }
+
+    public function articleAction()
+    {
+        return $this->render('AppBundle:Client:articles/article.html.twig');
+    }
+
+    public function dashboardClientAction()
+    {
+        return $this->render('AppBundle:Client:defaults/dashboard.html.twig');
     }
 }
