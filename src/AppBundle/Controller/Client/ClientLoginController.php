610f505d7b5c9981f2d8bcc20ba548234e025ec8
Awalin Yudhana <otoy.destroyed@gmail.com>
2018-01-08 19:51:09 +0700

    fix notification login

--- src/AppBundle/Controller/Client/ClientLoginController.php
+++ src/AppBundle/Controller/Client/ClientLoginController.php
@@ -10,6 +10,7 @@ namespace AppBundle\Controller\Client;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Controller\Api\ApiController;
 
 class ClientLoginController extends Controller
 {
@@ -17,6 +18,31 @@ class ClientLoginController extends Controller
     {
         if (true == $request->getSession()->get('isLogin')) {
             return $this->redirectToRoute('popem_client_dashboard');
+        }
+
+        if('POST' == $request->getMethod()) {
+        	$api = new ApiController();
+        	$options = [
+        		'username' => $request->get('username'),
+        		'password' => $request->get('password'),
+        	];
+
+        	$response = $api->doRequest(
+        		'POST',
+        		$this->container->getParameter('api_target').'/login',
+        		$options
+        	);
+
+        	if(true === $response['status']) {
+        		return $this->redirectToRoute('popem_client_dashboard');
+        	}else{
+        		$request->getSession()->getFlashBag()->add(
+        			'message',
+        			$response['data']['message']
+        		);
+
+        		return $this->redirect($request->headers->get('referer'));
+        	}
         }
 
         return $this->render('AppBundle:Client:defaults/login.html.twig');
